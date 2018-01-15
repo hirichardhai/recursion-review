5116164bb7b600e860538c3c558ffa919dc7ff08
hirichardhai <hirichardhai@gmail.com>
2018-01-15 13:14:54 -0800

    Completed primitive cases for JSON.stringify

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,32 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if(typeof obj === 'function' || typeof obj === undefined) {
+    return undefined;
+  }
+
+  // base case
+    // string
+  if(typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+
+  // for numbers
+  if(obj !== Object(obj)) {
+    return String(obj);
+  }
+
+  if(Arrray.isArray(obj)) {
+    obj.forEach(function(element){
+    
+    })  
+  }
+
+
+
+
+  
+  
+  return '' + obj;
 };
