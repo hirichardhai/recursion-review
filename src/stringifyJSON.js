95b6267b0afe1b8b9a3f91aa4fab1b3ae11a55f3
hirichardhai <hirichardhai@gmail.com>
2018-01-15 16:38:06 -0800

    Finished stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,34 +3,26 @@
 
 // but you don't so you're going to write it from scratch:
 
+
 var stringifyJSON = function(obj) {
-  // your code goes here
-  if(typeof obj === 'function' || typeof obj === undefined) {
-    return undefined;
-  }
-
-  // base case
-    // string
-  if(typeof obj === 'string') {
+  if (typeof obj === 'string') {
     return '"' + obj + '"';
+  } else if (typeof obj === 'undefined') {
+    return 'undefined';
+  } else if (Array.isArray(obj)) {
+    var stringArray = obj.map(function(obj) {
+      return stringifyJSON(obj);
+    });
+    return '[' + stringArray + ']';
+  } else if (obj && typeof obj === 'object') {
+    var objKeysAndValues = [];
+    for (var key in obj) {
+      if (stringifyJSON(obj[key]) === 'undefined') {
+        return '{}';
+      } 
+      objKeysAndValues.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+    } 
+    return '{' + objKeysAndValues.join(',') + '}';
   }
-
-
-  // for numbers
-  if(obj !== Object(obj)) {
-    return String(obj);
-  }
-
-  if(Arrray.isArray(obj)) {
-    obj.forEach(function(element){
-    
-    })  
-  }
-
-
-
-
-  
-  
   return '' + obj;
 };
